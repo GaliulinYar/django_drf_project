4c569187fc094e7eda0f9249a44502be68db9bd5
Yaroslav Galiulin <yaroslav66@list.ru>
2023-10-02 20:01:15 +0500

    Вьюшки для платежей

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
 
-from main.models import Courses, Lesson
-from main.serializers import CoursesSerializer, LessonSerializer
+from main.models import Courses, Lesson, Payments
+from main.serializers import CoursesSerializer, LessonSerializer, PaymentsSerializer
 
 
 class CoursesViewSet(viewsets.ModelViewSet):
@@ -39,3 +40,23 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 class LessonDestroyAPIView(generics.DestroyAPIView):
     """Generic для удаления одного урока Lesson"""
     queryset = Lesson.objects.all()
+
+
+# Вьюшки для платежей
+class PaymentsListAPIView(generics.ListAPIView):
+    """ Generic-класс для вывода списка платежей """
+
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    # Фильтр по полям
+    filterset_fields = ('course_pay', 'lesson_pay', 'owner', 'method',)
+    # Фильтр по датее
+    ordering_fields = ('date_pay',)
+
+
+class PaymentsRetrieveAPIView(generics.RetrieveAPIView):
+    """ Generic-класс для просмотра всех платежей одного юзера """
+
+    serializer_class = PaymentsSerializer
+    queryset = Payments.objects.all()
