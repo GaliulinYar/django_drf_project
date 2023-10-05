8d855d5dc491fe9760288312c4b0c125fa630e7b
Yaroslav Galiulin <yaroslav66@list.ru>
2023-10-05 21:10:01 +0500

    Настроили работу с уроками и платежами

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from main.models import Courses, Lesson, Payments
-from main.permissions import IsModeratorOrReadOnly, IsCourseOwner
+from main.permissions import IsModeratorOrReadOnly, IsCourseOwner, IsPaymentOwner
 from main.serializers import CourseSerializer, LessonSerializer, PaymentsSerializer
 from users.models import UserRoles
 
@@ -50,14 +50,30 @@ class CoursesViewSet(viewsets.ModelViewSet):
 class LessonCreateAPIView(generics.CreateAPIView):
     """Generic создания урока Lesson"""
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPaymentOwner]
+
+    def perform_create(self, serializer):
+        """ Запрещаем модераторам создавать уроки """
+        if self.request.user.role == UserRoles.MODERATOR:
+            raise PermissionDenied("Вы не можете создавать уроки")
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     """Generic просмотра всех уроков Lesson"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPaymentOwner]
+
+    def get_queryset(self):
+        """ Открываем доступ только владельцам и модераторам """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
@@ -65,18 +81,49 @@ class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
 
+    def get_queryset(self):
+        """ Открываем доступ только владельцам и модераторам """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
+
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     """Generic для обновления урока Lesson"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPaymentOwner]
+
+    def get_queryset(self):
+        """ Открываем доступ только владельцам и модераторам """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     """Generic для удаления одного урока Lesson"""
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPaymentOwner]
+
+    def get_queryset(self):
+        """ Открываем доступ только владельцам и модераторам """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=self.request.user)
+
+    def perform_destroy(self, instance):
+        """ Запрещаем модераторам удалять объект """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            raise PermissionDenied("Вам нельзя удалять уроки")
+        instance.delete()
 
 
 # Вьюшки для платежей
@@ -90,7 +137,15 @@ class PaymentsListAPIView(generics.ListAPIView):
     filterset_fields = ('course_pay', 'lesson_pay', 'owner', 'method',)
     # Фильтр по датее
     ordering_fields = ('date_pay',)
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModeratorOrReadOnly | IsPaymentOwner]
+
+    def get_queryset(self):
+        """ Посмотреть платежи могут только владельцы и модераторы """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Payments.objects.all()
+        else:
+            return Payments.objects.filter(owner=self.request.user)
 
 
 class PaymentsRetrieveAPIView(generics.RetrieveAPIView):
@@ -98,4 +153,12 @@ class PaymentsRetrieveAPIView(generics.RetrieveAPIView):
 
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsModeratorOrReadOnly | IsPaymentOwner]
+
+    def get_queryset(self):
+        """ Посмотреть платежи могут только владельцы и модераторы """
+
+        if self.request.user.role == UserRoles.MODERATOR:
+            return Payments.objects.all()
+        else:
+            return Payments.objects.filter(owner=self.request.user)
