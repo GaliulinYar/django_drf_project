ca5add168b56d158acbff9d47040e7c6c9e1448e
Yaroslav Galiulin <yaroslav66@list.ru>
2023-10-11 17:51:37 +0500

    Добавил подписки в админку

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from main.models import Courses, Lesson, Payments
+from main.models import Courses, Lesson, Payments, Subscription
 
 admin.site.register(Courses)
 admin.site.register(Lesson)
 admin.site.register(Payments)
+admin.site.register(Subscription)
